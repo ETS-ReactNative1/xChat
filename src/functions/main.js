e9666a84a68e0c446defd31fecf5af6c5b510047
LiLPandemio <markhaxx@gamil.com>
2021-05-05 23:53:54 +0200

    Fixed keys

--- src/functions/main.js
+++ src/functions/main.js
@@ -1,11 +1,11 @@
 export function getTextStoriesJSON() {
     const JSONTEXTSTORIES = [
-        { key: "14", profilePicURL: 'https://cataas.com/cat/says/114', time: '19:54', txt: 'K: 14 // Tengo comprar el pan LOL' },
-        { key: "13", profilePicURL: 'https://cataas.com/cat/says/113', time: '19:54', txt: 'K: 13 // Tengo comprar el pan LOL' },
-        { key: "12", profilePicURL: 'https://cataas.com/cat/says/112', time: '19:54', txt: 'K: 12 // Tengo comprar el pan LOL' },
-        { key: "11", profilePicURL: 'https://cataas.com/cat/says/111', time: '19:54', txt: 'K: 11 // Tengo comprar el pan LOL' },
-        { key: "24", profilePicURL: 'https://cataas.com/cat/says/124', time: '19:54', txt: 'K: 24 // Tengo comprar el pan LOL' },
-        { key: "23", profilePicURL: 'https://cataas.com/cat/says/123', time: '19:54', txt: 'K: 23 // Tengo comprar el pan LOL' },
+        { key: 1, profilePicURL: 'https://cataas.com/cat/says/1', time: '19:51', txt: 'Tengo que comprar 1 barra de pan, que perezaaaaa' },
+        { key: 2, profilePicURL: 'https://cataas.com/cat/says/2', time: '19:52', txt: 'Tengo que comprar 2 barras de pan, que perezaaaaa' },
+        { key: 3, profilePicURL: 'https://cataas.com/cat/says/3', time: '19:53', txt: 'Tengo que comprar 3 barras de pan, que perezaaaaa' },
+        { key: 4, profilePicURL: 'https://cataas.com/cat/says/4', time: '19:54', txt: 'Tengo que comprar 4 barras de pan, que perezaaaaa' },
+        { key: 5, profilePicURL: 'https://cataas.com/cat/says/5', time: '19:55', txt: 'Tengo que comprar 5 barras de pan, que perezaaaaa' },
+        { key: 6, profilePicURL: 'https://cataas.com/cat/says/6', time: '19:56', txt: 'Tengo que comprar 6 barras de pan, que perezaaaaa' },
     ]
     return JSONTEXTSTORIES;
 }
