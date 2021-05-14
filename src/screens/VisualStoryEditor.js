076cea295fa9c2af5b25a4927db090357a2c02a2
LiLPandemio <markhaxx@gmail.com>
2021-05-14 23:45:02 +0200

    Sticker picker added

--- src/screens/VisualStoryEditor.js
+++ src/screens/VisualStoryEditor.js
@@ -2,18 +2,32 @@
  * @Author: @LiLPandemio 
  * @Date: 2021-05-08 18:32:08 
  * @Last Modified by: @LiLPandemio
- * @Last Modified time: 2021-05-13 14:01:11
+ * @Last Modified time: 2021-05-14 23:44:45
  */
 import React, { Component } from 'react';
 import { View, Image, ImageBackground, FlatList, Alert, Dimensions, TouchableWithoutFeedback } from 'react-native'
-import { Avatar, Text, TouchableRipple } from 'react-native-paper';
+import { Avatar, Text, TouchableRipple, Portal, Modal, Provider } from 'react-native-paper';
 import { StickerPicker } from 'react-native-stickers';
 import PicSticker from '../components/PicSticker';
 import SuperIcon from '../components/SuperIcon'
 import Draggable from 'react-native-draggable';
+import { ScrollView } from 'react-native';
 
 
-
+/*
+let last = this.state.stickers.length;
+                            this.setState(prevState => ({
+                                stickers: [...prevState.stickers, {
+                                    id: last + 1,
+                                    uri: "https://cataas.com/cat/says/" + last + 1,
+                                    x: Dimensions.get('window').width / 2 - 75,
+                                    y: Dimensions.get('window').height / 2 - 75,
+                                    size: 150,
+                                    sizeController: false,
+                                }]
+                            }))
+                        }
+*/
 
 export default class VisualStoryEditor extends Component {
     constructor(props) {
@@ -23,15 +37,59 @@ export default class VisualStoryEditor extends Component {
 
         const textStickers = [];
 
+        const visibleStickerPicker = true;
+
+        this.state = { visibleStickerPicker }
         this.state = { textStickers };
         this.state = { stickers };
     }
     render() {
         const imgURI = this.props.route.params.cameraOutput.uri; //Debug purposes
+        const stickersOnline = [
+            { id: 1, uri: "https://cataas.com/cat/says/1" },
+            { id: 2, uri: "https://cataas.com/cat/says/2" },
+            { id: 3, uri: "https://cataas.com/cat/says/3" },
+            { id: 4, uri: "https://cataas.com/cat/says/4" },
+            { id: 5, uri: "https://cataas.com/cat/says/5" },
+            { id: 6, uri: "https://cataas.com/cat/says/6" },
+            { id: 7, uri: "https://cataas.com/cat/says/7" },
+            { id: 8, uri: "https://cataas.com/cat/says/8" },
+            { id: 9, uri: "https://cataas.com/cat/says/9" }
+        ]
         //console.log(imgURI)
         return (
             <View style={{ flex: 1 }}>
                 <ImageBackground source={{ uri: imgURI }} style={{ flex: 1, width: "100%", height: "100%", zIndex: 5, resizeMode: "cover", justifyContent: "center" }}>
+                    <Provider>
+                        <Portal>
+                            <Modal visible={this.state.visibleStickerPicker} onDismiss={() => { this.setState({ visibleStickerPicker: false }) }}
+                                contentContainerStyle={{ backgroundColor: '#888', padding: 20, width: "80%", marginLeft: "10%", borderRadius: 10, height: "60%" }}>
+                                <ScrollView>
+                                    {stickersOnline.map((stickerOnline, index) => (
+                                        <TouchableWithoutFeedback
+                                            onPress={() => {
+                                                //Add sticker   stickerOnline.uri
+                                                let last = this.state.stickers.length;
+                                                this.setState(prevState => ({
+                                                    stickers: [...prevState.stickers, {
+                                                        id: last + 1,
+                                                        uri: stickerOnline.uri,
+                                                        x: Dimensions.get('window').width / 2 - 75,
+                                                        y: Dimensions.get('window').height / 2 - 75,
+                                                        size: 150,
+                                                        sizeController: false,
+                                                        isVisible: true,
+                                                    }]
+                                                }))
+                                                this.setState({ visibleStickerPicker: false })
+                                            }}>
+                                            <Image style={{ width: 100, height: 100 }} source={{ uri: stickerOnline.uri }}></Image>
+                                        </TouchableWithoutFeedback>
+                                    ))}
+                                </ScrollView>
+                            </Modal>
+                        </Portal>
+                    </Provider>
                     <View style={{ flexDirection: "row", position: "absolute", top: 20, right: 210, borderRadius: 20, backgroundColor: "#0008", padding: 5, paddingHorizontal: 10 }}>
                         <TouchableRipple style={{}} onPress={() => {
                             this.props.navigation.navigate('MainScreen')
@@ -56,19 +114,7 @@ export default class VisualStoryEditor extends Component {
                             <SuperIcon type="Material" name="text-fields" color="white" size={40}></SuperIcon>
                         </TouchableRipple>
                         {/* this.state.stickers.push({ id: 112, uri: "https://cataas.com/cat/says/" + 112}); */}
-                        <TouchableRipple style={{ paddingHorizontal: 2.5, marginHorizontal: 2.5 }} onPress={() => {
-                            let last = this.state.stickers.length;
-                            this.setState(prevState => ({
-                                stickers: [...prevState.stickers, {
-                                    id: last + 1,
-                                    uri: "https://cataas.com/cat/says/" + last + 1,
-                                    x: Dimensions.get('window').width / 2 - 75,
-                                    y: Dimensions.get('window').height / 2 - 75,
-                                    size: 150,
-                                    sizeController: false,
-                                }]
-                            }))
-                        }} >
+                        <TouchableRipple style={{ paddingHorizontal: 2.5, marginHorizontal: 2.5 }} onPress={() => { this.setState({ visibleStickerPicker: true }); this.setState() }}>
                             <SuperIcon type="MaterialCommunity" name="sticker-plus-outline" color="white" size={40}></SuperIcon>
                         </TouchableRipple>
                     </View>
@@ -100,9 +146,9 @@ export default class VisualStoryEditor extends Component {
                                         stickers: newArray
                                     })
                                 }}
-                                onShortPressRelease={() => {}}
+                                onShortPressRelease={() => { }}
                             >
-                                <PicSticker daddystate={this.state} stickerid={sticker.id} setState={p=>{this.setState(p)}} size={sticker.size} sizeController={true} imgURI={sticker.uri} />
+                                <PicSticker daddystate={this.state} visible={sticker.isVisible} stickerid={sticker.id} setState={p => { this.setState(p) }} size={sticker.size} sizeController={true} imgURI={sticker.uri} />
                             </Draggable>
                         ))}
                     </View>
