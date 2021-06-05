916a55a3324f5051623717dd9817295310956458
LiLPandemio <markhaxx@gmail.com>
2021-06-05 23:32:01 +0200

    Reduced imports Starting story time run

--- src/screens/Story.js
+++ src/screens/Story.js
@@ -5,25 +5,36 @@
  * @Last Modified time: 2021-05-08 18:31:45 
  */
 import React, { Component } from 'react';
-import { View, Image, ImageBackground, FlatList, Alert, Dimensions, TouchableWithoutFeedback } from 'react-native'
-import { Avatar, Text, TouchableRipple, ProgressBar } from 'react-native-paper';
-import { StickerPicker } from 'react-native-stickers';
-import PicSticker from '../components/PicSticker';
-import SuperIcon from '../components/SuperIcon'
+import { View, ImageBackground } from 'react-native'
+import { ProgressBar } from 'react-native-paper';
 //props.storyImageContent has the image!
-const Story = (props) => {
-    const imgURI = props.route.params.storyImageContent
-    return (
+
+export class Story extends Component {
+    constructor(props) {
+        super(props);
+        const progress = 0;
+        this.state = { progress };
+    }
+    render() {
+        return (
         <View style={{ flex: 1 }}>
-            <ImageBackground source={{ uri: props.route.params.storyImageContent }} style={{ flex: 1, width: "100%", height: "100%", resizeMode: "cover" }}>
+            <ImageBackground source={{ uri: this.props.route.params.storyImageContent }} style={{ flex: 1, width: "100%", height: "100%", resizeMode: "cover" }}>
                 <View>
                     <View style={{ width: "95%", marginHorizontal: "2.5%", marginTop: 7.5}}>
-                        <ProgressBar progress={0.5} style={{borderRadius: 20}} color="#eee"></ProgressBar>
+                        <ProgressBar progress={this.state.progress} style={{borderRadius: 20}} color="#eee"></ProgressBar>
                     </View>
                 </View>
             </ImageBackground>
         </View>
-    )
+        )
+    }
+    componentDidMount() { 
+        this._mounted = true;
+        for (let i; i < 1; i+= 0.1){
+            this.setState({ progress: i })
+        }
+    }
+      
 }
 
 export default Story
