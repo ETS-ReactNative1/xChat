54575ebcc67e93c0ab2a542df580619da95e67e8
LiLPandemio <markhaxx@gmail.com>
2021-08-02 16:41:16 +0200

    Upload Text Story

--- src/screens/TextStoryCreator.js
+++ src/screens/TextStoryCreator.js
@@ -6,16 +6,55 @@
  */
 import React from 'react'
 import { TextInput, Button, Appbar } from 'react-native-paper'
+import Snackbar from 'react-native-snackbar';
 import { View, Text } from 'react-native'
 import SuperIcon from '../components/SuperIcon'
 import { StackActions, useNavigation } from '@react-navigation/native';
+import { UploadQuicktext } from '../functions/main';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 const TextStoryCreator = () => {
     const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
     const popAction = StackActions.pop(1);
     const navigation = useNavigation();
     const [text, setText] = React.useState('');
+    async function UploadQuicktext() {
+        const session = await EncryptedStorage.getItem("user_session");
+        const sesdata = JSON.parse(session);
+        //---------------------------------------------------------------------------- sesdata.token and text have the necessary info
 
+        fetch('http://192.168.1.200/index.php', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                do: 'uploadquicktext',
+                token: sesdata.token,
+                quicktext: text,
+            })
+        })
+            .then(response => response.json())
+            .then(response => {
+                if (response.response === "StoryUploaded") {
+                    Snackbar.show({
+                        text: 'QuickText Subido!',
+                        duration: Snackbar.LENGTH_SHORT,
+                    });
+                    setText("");
+                    navigation.dispatch(popAction)
+                } else {
+                    Snackbar.show({
+                        text: 'Something went wrong :(',
+                        duration: Snackbar.LENGTH_SHORT,
+                    });
+                }
+            })
+            .catch(err => console.error(err));
+
+        //----------------------------------------------------------------------------
+    }
     return (
         <View style={{ flex: 1 }}>
             <Appbar.Header style={{ backgroundColor: "#34803d" }}>
@@ -26,8 +65,8 @@ const TextStoryCreator = () => {
                 marginHorizontal: "5%"
             }}>
                 {/* MAIN VIEW */}
-                <TextInput onChangeText={text => setText(text)} maxLength={1200} multiline={true} numberOfLines={6} style={{marginTop: 10}} mode='outlined' label="QuickText"></TextInput>
-                <Button style={{marginTop: 10}} mode="contained" disabled={false} onPress={() => console.log('Pressed')}>Publicar</Button>
+                <TextInput onChangeText={text => setText(text)} maxLength={1200} multiline={true} numberOfLines={6} style={{ marginTop: 10 }} mode='outlined' label="QuickText"></TextInput>
+                <Button style={{ marginTop: 10 }} mode="contained" disabled={false} onPress={() => UploadQuicktext()}>Publicar</Button>
             </View>
         </View>
     )
